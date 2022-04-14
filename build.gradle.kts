18b69ae87eac5c49bb3705f07451c22e0132e09f
Alpha <alphakr93@outlook.com>
2022-04-14 15:36:45 +0900

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,8 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
